a11c5d23755a26e95cdd13a868164eb83149bcd0
Mahidhar <mahidhar96@gmail.com>
2020-07-09 10:22:56 -0700

    start

---
+++ imports/ui/components/user-button-menu/component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const UserButtonMenu = ({ props }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <span style={{ color: "white" }}>{props.userName}</span>
+      </Button>
+      <Menu
+        id="user-button-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+export default UserButtonMenu;
